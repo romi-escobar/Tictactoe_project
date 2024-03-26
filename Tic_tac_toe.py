2ed2047ea450c740bb27649dbf6a73678331ef16
romi_escobar <romi.escobar1615@gmail.com>
2024-03-26 12:02:56 -0400

    a simple tic_tac_toe game

---
+++ Tic_tac_toe.py
@@ -0,0 +1,26 @@
+# A list of lists
+board = [
+    ['','',''],
+    ['','',''],
+    ['','','']
+]
+
+# Funtion: displays the tic tac toe board in the console
+def display_board():
+    for row in board:
+        print('|'.join(row))
+        print('-'*5)
+
+
+
+
+
+
+# Enter players name
+#player_1 = input("Enter a name for player 1: ")
+#player_2 = input("Enter a name for player 2: ")
+
+#print("Ok! the players are", player_1, "and", player_2)
+#print(player_1, "starts the game")
+
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+version
+1: make a simple titactoe game, that displays in the console, and has two players
+1.00: player 1 starts the game
+1.01: from a random selection a player is choose to start the game
+
+Learning about
+
+print()
+input()
